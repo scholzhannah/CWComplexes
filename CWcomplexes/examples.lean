fe8daa421a91cf771875e6b1aa7f2b221865e279
Hannah Scholz <hannah@scholzbande.de>
2024-08-18 17:03:11 +0200

    finished interval -1 1 start on general interval

--- CWcomplexes/examples.lean
+++ CWcomplexes/examples.lean
@@ -1,4 +1,4 @@
-import CWcomplexes.Definition
+import CWcomplexes.Constructions
 
 set_option autoImplicit false
 set_option linter.unusedVariables false
@@ -10,120 +10,75 @@ namespace CWComplex
 
 variable {X : Type*} [t : TopologicalSpace X] [T2Space X]
 
-instance instCWComplexempty_test : CWComplex (∅ : Set X) := CWComplexFinite
+instance instCWComplexempty : CWComplex (∅ : Set X) := CWComplexFinite
   (cell := fun _ ↦ PEmpty)
   (map := fun _ i ↦ i.elim)
   (finitelevels := by rw [Filter.eventually_atTop]; use 0; exact fun _ _ ↦ instIsEmptyPEmpty)
   (finitecells := fun n ↦ Finite.of_fintype ((fun x ↦ PEmpty) n))
-  (source_eq := fun _ _ ↦ by contradiction)
-  (cont := fun _ _ ↦ by contradiction)
-  (cont_symm := fun _ _ ↦ by contradiction)
-  (pairwiseDisjoint' := by rw [PairwiseDisjoint, Set.Pairwise]; intro ⟨_, _⟩; contradiction)
-  (mapsto := fun _ _ ↦ by contradiction)
+  (source_eq := fun _ i ↦ i.elim)
+  (cont := fun _ i ↦ i.elim)
+  (cont_symm := fun _ i ↦ i.elim)
+  (pairwiseDisjoint' := by rw [PairwiseDisjoint, Set.Pairwise]; intro ⟨_, i⟩; exact i.elim)
+  (mapsto := fun _ i ↦ i.elim)
   (union' := by simp [iUnion_of_empty, iUnion_empty])
-
--- is this really the theorem that should be used?
-example (i : PEmpty) : False := by exact Aesop.BuiltinRules.pEmpty_false i
-
-instance instCWComplexempty : CWComplex (∅ : Set X) where
-  cell n := PEmpty
-  map _ i := (Aesop.BuiltinRules.pEmpty_false i).elim
-  source_eq n i := by contradiction
-  cont n i := by contradiction
-  cont_symm n i := by contradiction
-  pairwiseDisjoint' := by
-    rw [PairwiseDisjoint, Set.Pairwise]
-    intro ⟨_, _⟩
-    contradiction
-  mapsto n i := by contradiction
-  closed' A := by
-    intro Aempty
-    rw [subset_empty_iff] at Aempty
-    constructor
-    · intro _ _ _
-      contradiction
-    · intro _
-      rw [Aempty]
-      exact isClosed_empty
-  union' := by simp only [iUnion_of_empty, iUnion_empty]
 
 variable [T1Space X]
 
-instance instCWComplexsingleton (x : X) : CWComplex {x} where
-  cell n := match n with
+instance instCWComplexsingleton (x : X) : CWComplex {x} := CWComplexFinite
+  (cell := fun n ↦ match n with
     | 0 => PUnit
-    | (m + 1) => PEmpty
-  map n i := match n with
+    | (m + 1) => PEmpty)
+  (map := fun n i ↦
+    match n with
     | 0 => PartialEquiv.const ![] x
-    | (m + 1) => by contradiction
-  source_eq n i := match n with
+    | (m + 1) => i.elim)
+  (finitelevels := by
+    rw [Filter.eventually_atTop]
+    use 1
+    intro b beq1
+    simp only
+    split
+    · contradiction
+    · exact instIsEmptyPEmpty)
+  (finitecells := fun n ↦ match n with
+    | 0 => Finite.of_fintype _
+    | (m + 1) => Finite.of_fintype _)
+  (source_eq := fun n i ↦ match n with
     | 0 => by
-      simp only [PartialEquiv.const, Matrix.empty_eq, closedBall, dist_self, zero_le_one, setOf_true]
-      rw [Set.eq_univ_iff_forall]
+      simp only [PartialEquiv.const, closedBall, Matrix.empty_eq, dist_self, zero_le_one,
+        setOf_true, eq_univ_iff_forall]
       intro _
       simp only [Matrix.empty_eq, mem_singleton_iff]
-    | (m + 1) => by contradiction
-  cont n i := match n with
+    | (m + 1) => i.elim)
+  (cont := fun n i ↦ match n with | 0 => continuousOn_const | (m + 1) => i.elim)
+  (cont_symm := fun n i ↦ match n with
     | 0 => continuousOn_const
-    | (m + 1) => by contradiction
-  cont_symm n i := match n with
-    | 0 => continuousOn_const
-    | (m + 1) => by contradiction
-  pairwiseDisjoint' := by
+    | (m + 1) => i.elim)
+  (pairwiseDisjoint' := by
     rw [PairwiseDisjoint, Set.Pairwise]
-    intro ⟨n, j⟩ _ ⟨m, i⟩ _ ne
-    rcases n with rfl | n
-    · rcases m with rfl | m
-      · contradiction
-      contradiction
-    contradiction
-  mapsto n i := match n with
-    | 0 => by
-      let I m : Finset (match m with | 0 => PUnit.{u_1 + 1}| (m + 1) => PEmpty.{u_1 + 1}) := match m with
-        | 0 => Finset.univ
-        | (m + 1) => Finset.univ
-      use I
-      simp only [Matrix.zero_empty, sphere_zero_dim_empty, not_lt_zero', iUnion_of_empty,
-        iUnion_empty, mapsTo_empty_iff]
-    | (m + 1) => by contradiction
-  closed' A Asub := by
-    constructor
-    · intro closedA n j
-      rcases n with rfl | n
-      · simp [PartialEquiv.const]
-        exact IsClosed.inter closedA isClosed_singleton
-      · contradiction
-    · intro _
-      exact Set.Subsingleton.isClosed (Set.subsingleton_of_subset_singleton Asub)
-  union' := by
+    exact fun ⟨n, j⟩ _ ⟨m, i⟩ _ ne ↦  match n with
+      | 0 => match m with
+        | 0 => (ne rfl).elim
+        | (l + 1) => i.elim
+      | (l + 1) => j.elim)
+  (mapsto := fun n i ↦ match n with
+    | 0 => by simp only [Matrix.zero_empty, sphere_zero_dim_empty, not_lt_zero', iUnion_of_empty,
+      iUnion_empty, mapsTo_empty_iff]
+    | (m + 1) => i.elim)
+  (union' := by
     ext y
+    simp only [mem_iUnion, mem_singleton_iff]
     constructor
-    · simp_rw [mem_iUnion]
-      intro ⟨n, j, hn⟩
-      rcases n with rfl | n
-      · simp only [PartialEquiv.const, Function.const_apply, Matrix.zero_empty,
-        nonempty_closedBall, zero_le_one, Nonempty.image_const, mem_singleton_iff] at hn ⊢
-        exact hn
-      · contradiction
+    · exact fun ⟨n, j, hn⟩ ↦  match n with
+        | 0 => by simp_all only [PartialEquiv.const, Function.const_apply, Matrix.zero_empty,
+          nonempty_closedBall, zero_le_one, Nonempty.image_const, mem_singleton_iff]
+        | (m + 1) => j.elim
     · intro eq
-      rw [mem_singleton_iff] at eq
-      subst eq
-      simp only [mem_iUnion]
       use 0, default
-      simp only [PartialEquiv.const, Function.const_apply, Matrix.zero_empty, nonempty_closedBall,
-        zero_le_one, Nonempty.image_const, mem_singleton_iff]
+      simp only [eq, PartialEquiv.const, Function.const_apply, Matrix.zero_empty,
+        nonempty_closedBall, zero_le_one, Nonempty.image_const, mem_singleton_iff])
 
-example : (sphere 0 1 : Set ℝ) = {-1, 1} := by
-  ext x
-  simp [abs]
-  aesop
-
-
-
-  sorry
-
-
-instance instCWComplexinterval_test: CWComplex (Set.Icc (-1) 1 : Set ℝ) := CWComplexFinite
+instance instCWComplexstandardinterval_test: CWComplex (Icc (-1) 1 : Set ℝ) := CWComplexFinite
   (cell := fun n ↦ match n with
     | 0 => Fin 2
     | 1 => Fin 1
@@ -146,8 +101,8 @@ instance instCWComplexinterval_test: CWComplex (Set.Icc (-1) 1 : Set ℝ) := CWC
     intro n nge2
     simp only
     split
-    · simp only [ge_iff_le, nonpos_iff_eq_zero, OfNat.ofNat_ne_zero] at nge2
-    · simp only [ge_iff_le, Nat.not_ofNat_le_one] at nge2
+    · contradiction
+    · contradiction
     · exact instIsEmptyEmpty
   )
   (finitecells := fun n ↦ match n with
@@ -234,9 +189,21 @@ instance instCWComplexinterval_test: CWComplex (Set.Icc (-1) 1 : Set ℝ) := CWC
       | 0 => by simp [sphere_zero_dim_empty]
       | 1 => by simp [sphere_zero_dim_empty]
     | 1 => by
-      intro x
-      simp [mapsTo', IsometryEquiv.image_sphere]
-      sorry
+      simp_rw [mapsTo', Equiv.toPartialEquivOfImageEq_apply, IsometryEquiv.coe_toEquiv,
+        IsometryEquiv.image_sphere, IsometryEquiv.funUnique_toFun, Pi.zero_apply, Nat.lt_one_iff, iUnion_iUnion_eq_left, Matrix.zero_empty]
+      intro x xmem
+      simp only [mem_sphere_iff_norm, norm, sub_zero] at xmem
+      replace xmem := eq_or_eq_neg_of_abs_eq xmem
+      rw [mem_iUnion]
+      rcases xmem with xeq | xeq
+      · subst x
+        use 1
+        simp only [PartialEquiv.const, Function.const_one, Pi.one_apply, nonempty_closedBall,
+          zero_le_one, Nonempty.image_const, mem_singleton_iff]
+      · subst x
+        use 0
+        simp only [PartialEquiv.const, Function.const_apply, nonempty_closedBall, zero_le_one,
+          Nonempty.image_const, mem_singleton_iff]
     | (m + 2) => i.elim
   )
   (union' := by
@@ -257,6 +224,10 @@ instance instCWComplexinterval_test: CWComplex (Set.Icc (-1) 1 : Set ℝ) := CWC
         IsometryEquiv.image_closedBall, IsometryEquiv.funUnique_toFun, Pi.zero_apply,
         Real.closedBall_eq_Icc, zero_sub, zero_add]
   )
+
+instance instCWComplexinterval_test (a b : ℝ) (lt : a < b) : CWComplex (Icc a b : Set ℝ) :=
+  CWComplex_of_Homeomorph (Icc (-1) 1 : Set ℝ) (Icc a b)
+  (affineHomeomorph ((b - a) / 2) ((a + b) / 2) (sorry)) (sorry)
 
 instance instCWComplexinterval: CWComplex (Set.Icc (-1) 1 : Set ℝ) where
   cell :=
