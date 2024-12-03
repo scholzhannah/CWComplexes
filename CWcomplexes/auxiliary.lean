ff266b4c9d5c51ca19fae6e31eb0f2fec8d2cc64
Hannah Scholz <hannah@scholzbande.de>
2024-12-03 17:56:26 +0100

    some auxiliary lemma

--- CWcomplexes/auxiliary.lean
+++ CWcomplexes/auxiliary.lean
@@ -4,7 +4,6 @@ import Mathlib.Topology.Sets.Compacts
 import Mathlib.Topology.MetricSpace.Isometry
 import Mathlib.Analysis.Normed.Group.Basic
 import Mathlib.Data.Fintype.Lattice
-import Mathlib.Data.Fintype.Sum
 
 /-!
 # Auxiliary lemmas and definitions
@@ -79,9 +78,7 @@ lemma isClosed_left_of_isClosed_union {X : Type*} [TopologicalSpace X] {A B : Se
     (hAB : SeparatedNhds A B) (hAB' : IsClosed (A ∪ B)) : IsClosed A := by
   obtain ⟨U, V, hU, hV, hAU, hBV, hUV⟩ := hAB
   rw [← isOpen_compl_iff] at hAB' ⊢
-  suffices Aᶜ = (A ∪ B)ᶜ ∪ V by
-    rw [this]
-    exact hAB'.union hV
+  suffices Aᶜ = (A ∪ B)ᶜ ∪ V from this ▸ hAB'.union hV
   have : B ∩ Vᶜ = ∅ := by aesop
   rw [← compl_inj_iff, Set.compl_union, compl_compl, compl_compl, Set.union_inter_distrib_right,
     this, Set.union_empty, Set.left_eq_inter, Set.subset_compl_comm]
@@ -91,7 +88,10 @@ lemma isClosed_right_of_isClosed_union {X : Type*} [TopologicalSpace X] {A B : S
     (hAB : SeparatedNhds A B) (closedAB : IsClosed (A ∪ B)) : IsClosed B :=
   isClosed_left_of_isClosed_union hAB.symm (Set.union_comm _ _ ▸ closedAB)
 
-open Finset
+lemma isClosed_union_iff_isClosed {X : Type*} [TopologicalSpace X] {A B : Set X}
+    (hAB : SeparatedNhds A B) : IsClosed (A ∪ B) ↔ IsClosed A ∧ IsClosed B :=
+  ⟨fun h ↦ ⟨isClosed_left_of_isClosed_union hAB h, isClosed_right_of_isClosed_union hAB h⟩,
+    fun ⟨h1, h2⟩ ↦ h1.union h2⟩
 
 instance {α β : Sort*} [Finite α] [Finite β] : Finite (α ⊕' β) := by
   sorry
