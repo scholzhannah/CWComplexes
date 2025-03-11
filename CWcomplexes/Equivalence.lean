f6f88cb42c2b8240e8ac33454754cf1d12fbe644
Hannah Scholz <hannah@scholzbande.de>
2025-03-11 13:20:45 +0100

    started definition in equivalence.lean

--- CWcomplexes/Equivalence.lean
+++ CWcomplexes/Equivalence.lean
@@ -1,8 +1,23 @@
 import Mathlib.Topology.CWComplex.Classical.Basic
 import Mathlib.Topology.CWComplex.Abstract.Basic
 
-open Set
+noncomputable section
+
+open Set HomotopicalAlgebra
 
 variable {X : Type*} [TopologicalSpace X] (C D : Set X) [Topology.RelCWComplex C D]
 
-def direction1 : TopCat.RelativeCWComplex (X := TopCat.of D) (Y := TopCat.of C) sorry := sorry
+def direction1 : TopCat.RelativeCWComplex (X := TopCat.of D) (Y := TopCat.of C)
+    (TopCat.ofHom (ContinuousMap.inclusion Topology.RelCWComplex.base_subset_complex))where
+  F := {
+    obj n := TopCat.of (Topology.RelCWComplex.skeletonLT C n)
+    map h := TopCat.ofHom (ContinuousMap.inclusion (Topology.RelCWComplex.skeletonLT_mono sorry))
+    map_id := sorry
+    map_comp := sorry
+  }
+  isoBot := sorry
+  isWellOrderContinuous := sorry
+  incl := sorry
+  isColimit := sorry
+  fac := sorry
+  attachCells := sorry
